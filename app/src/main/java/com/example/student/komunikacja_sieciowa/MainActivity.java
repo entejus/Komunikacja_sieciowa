f2776ff1b3e9e1c4eb4a51bdf6e082a592c3d838
entejus <entejus@users.noreply.github.com>
2018-05-19 14:21:30 +0200

    Dodane sprawdzanie uprawnien

--- app/src/main/java/com/example/student/komunikacja_sieciowa/MainActivity.java
+++ app/src/main/java/com/example/student/komunikacja_sieciowa/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.student.komunikacja_sieciowa;
 
+import android.Manifest;
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +13,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static int MOJE_UPRAWNIENIA = 2018;
     private Button pobierzInfo;
     private Button pobierzPlik;
     private TextView adres;
@@ -26,22 +31,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pobierzPlik = (Button) findViewById(R.id.pobierz_plik);
+        pobierzPlik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MOJE_UPRAWNIENIA);
+                } else {
+                    Log.d("Uprawnienia", "Mam uprawnienia");
+                    UslugaPobieranie.uruchomPobieranie(MainActivity.this, adres.getText().toString());
+                }
+            }
+        });
         adres =
                 (TextView) findViewById(R.id.adres);
+        adres.setText("http://www.tapeta-lampart-grafika-3.na-pulpit.com/zdjecia/lampart-grafika-3.jpeg");
         pobierzInfo = (Button) findViewById(R.id.pobierz_info);
-        pobierzPlik = (Button) findViewById(R.id.pobierz_plik);
+
         pobierzInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 informacjeOPliku();
             }
         });
-        pobierzPlik.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UslugaPobieranie.uruchomPobieranie(MainActivity.this,adres.getText().toString());
-            }
-        });
+
     }
 
     private void informacjeOPliku() {
@@ -50,7 +64,26 @@ public class MainActivity extends AppCompatActivity {
         Log.d("PI", "zadanie uruchomione");
     }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d("Uprawnienia", "Sprawdzenie uprawnien, liczba uprawnien: " + permissions.length);
+        switch (requestCode) {
+            case MOJE_UPRAWNIENIA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("Uprawnienia", "Otrzymano uprawnienie: " + permissions[0]);
+                    pobierzPlik = (Button) findViewById(R.id.pobierz_plik);
+                    pobierzPlik.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            UslugaPobieranie.uruchomPobieranie(MainActivity.this, adres.getText().toString());
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "Nie mogę pobrać pliku bez uprawnień", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 
     class ZadanieAsynchroniczne extends AsyncTask<String, Void, String[]> {
 
